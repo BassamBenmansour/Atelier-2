8ef4e599cbe232b52eb6da6ae046a4291206994f
BassamBenmansour <162790375+BassamBenmansour@users.noreply.github.com>
2024-04-04 13:27:03 +0000

    Delete J2EE_ACT02_JPA_USERS_ROLES directory

--- J2EE_ACT02_JPA_USERS_ROLES/src/main/java/com/example/jpamanytomany/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.jpamanytomany.repositories;
-
-import com.example.jpamanytomany.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,String> {
-User findByUsername(String userName);
-}
