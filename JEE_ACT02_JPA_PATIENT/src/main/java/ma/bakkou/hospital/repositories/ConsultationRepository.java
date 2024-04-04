b9531963e99a040b33d62b24e4abb7df2d2e5a61
BassamBenmansour <162790375+BassamBenmansour@users.noreply.github.com>
2024-04-04 13:02:55 +0000

    Delete JEE_ACT02_JPA_PATIENT/src/main/java/ma/bakkou/hospital directory

--- JEE_ACT02_JPA_PATIENT/src/main/java/ma/bakkou/hospital/repositories/ConsultationRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.bakkou.hospital.repositories;
-
-import ma.bakkou.hospital.entities.Consultation;
-import ma.bakkou.hospital.entities.Medecin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConsultationRepository extends JpaRepository<Consultation,Long> {
-}
